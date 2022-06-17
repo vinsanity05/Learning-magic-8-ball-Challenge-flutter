eaedf91c392c5c464eeeefccc4f6df43c6278362
Vince V <chroviver3@gmail.com>
2022-06-17 15:39:26 +0100

    Creating a magic 8 ball application. This is a challenge from the Flutter lesson when creating a Dice App.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Magic8BallPage(),
       ),
     );
+
+class Magic8BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      backgroundColor: Colors.blue,
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void _randomizeNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+      print(ballNumber);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: _randomizeNumber,
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
